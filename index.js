9d57f506edb7a005afe126608ae6042807461eb9
ShawnYS-codemtl <syatsin@gmail.com>
2025-11-10 10:24:14 -0500

    feat: implement footer icons

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const toggleButton = document.querySelector(".nav-toggle");
 const navLinks = document.querySelector(".nav-links");
+const yearEl = document.getElementById('year')
 
 toggleButton.addEventListener("click", () => {
   navLinks.classList.toggle("show");
 });
+
+const year = new Date().getFullYear()
+yearEl.textContent = ` © ${year} Shawn Yat Sin`
